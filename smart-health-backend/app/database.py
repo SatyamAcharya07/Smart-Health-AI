a3b72146e331118b83ad21dbf05c41bfb186862f
Satyam Acharya <asatyam728@gmail.com>
2026-07-07 00:10:00 +0530

    Prepare backend for cloud deployment

--- smart-health-backend/app/database.py
+++ smart-health-backend/app/database.py
@@ -1,22 +1,31 @@
 import os
+import json
 from pathlib import Path
 
 import firebase_admin
 from dotenv import load_dotenv
 from firebase_admin import credentials, firestore
 
-# smart-health-backend/
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Load .env
 load_dotenv(BASE_DIR / ".env")
 
-key_name = os.getenv("FIREBASE_KEY_PATH")
+# -------------------------------
+# Local development
+# -------------------------------
+firebase_key_path = os.getenv("FIREBASE_KEY_PATH")
 
+# -------------------------------
+# Render deployment
+# -------------------------------
+firebase_credentials = os.getenv("FIREBASE_CREDENTIALS")
 
-key_path = BASE_DIR / key_name
-
-cred = credentials.Certificate(str(key_path))
+if firebase_credentials:
+    cred_dict = json.loads(firebase_credentials)
+    cred = credentials.Certificate(cred_dict)
+else:
+    key_path = BASE_DIR / firebase_key_path
+    cred = credentials.Certificate(str(key_path))
 
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
