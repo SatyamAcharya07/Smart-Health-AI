1479281b8495071a29ef4ce18bc78e835f1a6bba
Satyam Acharya <asatyam728@gmail.com>
2026-07-07 01:05:18 +0530

    Allow deployed frontend

--- smart-health-backend/app/main.py
+++ smart-health-backend/app/main.py
@@ -18,7 +18,8 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "https://smart-health-ai-delta.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
